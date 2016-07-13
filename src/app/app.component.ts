cd859314fcf9f4e3899192ed59fc5fd2a077d558
Marcelo <mocr@gft.com>
2016-07-13 16:15:53 +0200

    refactor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,48 +1,24 @@
-import { Component } from '@angular/core'; 
-import {MdToolbar} from '@angular2-material/toolbar';
-import {MdButton} from '@angular2-material/button';
-import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
-import {MD_LIST_DIRECTIVES} from '@angular2-material/list';
+import { Component } from '@angular/core';  
 import {MdIcon, MdIconRegistry} from '@angular2-material/icon';
 import { ROUTER_DIRECTIVES, Router } from '@angular/router';
 import { UsersComponent } from './users'; 
 import {HTTP_PROVIDERS} from '@angular/http';
+import { ToolbarComponent, NavbarComponent } from './shared/index';
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.css'],
-  directives: [ 
-    MdToolbar,
-    MdButton,
-    MD_SIDENAV_DIRECTIVES,
-    MD_LIST_DIRECTIVES,
+  directives: [  
     MdIcon,
-    ROUTER_DIRECTIVES
+    ROUTER_DIRECTIVES,
+    NavbarComponent,
+    ToolbarComponent
   ],
   providers: [  MdIconRegistry, HTTP_PROVIDERS]
 })
  
-export class AppComponent{
-  views: Object[] = [
-    {
-      name: "My Account",
-      description: "Edit my account information",
-      icon: "assignment_ind"
-    },
-    {
-      name: "Potential dates",
-      description: "Find your soulmate!",
-      icon: "pets"
-    }
-  ];
-  
-  constructor (private _router:Router) {    
-  }
-  
-  navigate(){
-    this._router.navigate(['/users']);
-  }
+export class AppComponent{  
 }
 
